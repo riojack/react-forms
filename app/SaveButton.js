dd40b45f819012e9e9d011e6579075415d18991c
riojack <ayjay122@gmail.com>
2017-02-06 07:44:50 -0600

    Save button created and tested.

---
+++ app/SaveButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SaveButton extends React.Component {
+    render() {
+        return <div>
+            <button onClick={this.props.onSave}>Save</button>
+        </div>;
+    }
+}
+
+export default SaveButton;
---
+++ test/SaveButton.spec.js
@@ -0,0 +1,44 @@
+import SaveButton from '../app/SaveButton';
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import {stub, assert} from 'sinon';
+
+describe('Save button tests', () => {
+    let props,
+        element;
+
+    beforeEach(() => {
+        props = {
+            onSave: stub()
+        };
+        element = shallow(<SaveButton {...props} />)
+    });
+
+    it('should be a div', () => {
+        expect(element.type()).to.equal('div');
+    });
+
+    it('should contain one child that is a button element', () => {
+        let children = element.children(),
+            button = children.first();
+
+        expect(children).to.have.length(1);
+        expect(button.type()).to.equal('button');
+    });
+
+    it('should have the text "Save" inside the button', () => {
+        let button = element.children('button').first(),
+            textChild = button.children().text();
+
+        expect(textChild).to.equal('Save');
+    });
+
+    it('should call props.onSave when the button is clicked', () => {
+        let button = element.children('button');
+
+        button.simulate('click');
+
+        assert.calledOnce(props.onSave);
+    });
+});
